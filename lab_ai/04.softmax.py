d9afbedfde4843195e709604d832f44ac808b134
jungeuno <jungeuno76@gmail.com>
2022-05-25 20:34:55 +0900

    Create 04.softmax.py

---
+++ lab_ai/04.softmax.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Embedding(3, 5),
+    tf.keras.layers.SimpleRNN(4),
+    tf.keras.layers.Dense(6),
+    tf.keras.layers.Softmax()
+])
+
+model.summary()
